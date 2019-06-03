17dc50b7ca37a3ad43ff910f897cc492ba986964
fede379 <fedeloe379@gmail.com>
2019-06-03 15:32:30 -0300

    testing fix triple quote

--- cinaptic/cinaptic/api/library/semantic/CypherQueries.py
+++ cinaptic/cinaptic/api/library/semantic/CypherQueries.py
@@ -5,23 +5,23 @@ import logging
 
 class CypherQueries:
     def closeness_algo(self, nameGraph, relation, improved):
-        query = f'''CALL algo.closeness.stream('MATCH (e:Entidad) WHERE e.idGraph="{nameGraph}" RETURN id(e) as id',
-                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph="{nameGraph}" AND e2.idGraph="{nameGraph}" RETURN id(e1) as source, id(e2) as target',
-                    {{graph:'cypher', write: true {", improved: true" if improved else ""} }})
+        query = f'''CALL algo.closeness.stream('MATCH (e:Entidad) WHERE e.idGraph=\"{nameGraph}\" RETURN id(e) as id',
+                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph=\"{nameGraph}\" AND e2.idGraph=\"{nameGraph}\" RETURN id(e1) as source, id(e2) as target',
+                    {{graph:'cypher', write: true {', improved: true' if improved else ""} }})
                     YIELD nodeId, centrality
                     RETURN algo.getNodeById(nodeId).name as Entidad, centrality
                     ORDER BY centrality DESC
                     LIMIT 20;'''                    
         results, header = db.cypher_query(query)
         meta = f"Closeness idGraph {nameGraph} relation {relation} improved {improved}"
-        print(meta, results)
+        #print(meta, results)
         logging.info(meta)
         self.exportCsv("_".join(meta.split(" ")), header, results)
         return results, header
 
     def closeness_harmonic_algo(self, nameGraph, relation):
-        query = f'''CALL algo.closeness.harmonic.stream('MATCH (e:Entidad) WHERE e.idGraph="{nameGraph}" RETURN id(e) as id',
-                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph="{nameGraph}" AND e2.idGraph="{nameGraph}" RETURN id(e1) as source, id(e2) as target',
+        query = f'''CALL algo.closeness.harmonic.stream('MATCH (e:Entidad) WHERE e.idGraph=\"{nameGraph}\" RETURN id(e) as id',
+                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph=\"{nameGraph}\" AND e2.idGraph=\"{nameGraph}\" RETURN id(e1) as source, id(e2) as target',
                     {{graph:'cypher', write: true}})
                     YIELD nodeId, centrality
                     RETURN algo.getNodeById(nodeId).name as Entidad, centrality
@@ -29,14 +29,14 @@ class CypherQueries:
                     LIMIT 20;'''
         results, header = db.cypher_query(query)
         meta = f"Harmonic Closeness idGraph {nameGraph} relation {relation}"
-        print(meta, results)
+        #print(meta, results)
         logging.info(meta)
         self.exportCsv("_".join(meta.split(" ")), header, results)
         return results, header
 
     def betweenness_algo(self, nameGraph, relation):
-        query = f'''CALL algo.betweenness.stream('MATCH (e:Entidad) WHERE e.idGraph="{nameGraph}" RETURN id(e) as id',
-                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph="{nameGraph}" AND e2.idGraph="{nameGraph}" RETURN id(e1) as source, id(e2) as target',
+        query = f'''CALL algo.betweenness.stream('MATCH (e:Entidad) WHERE e.idGraph=\"{nameGraph}\" RETURN id(e) as id',
+                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph=\"{nameGraph}\" AND e2.idGraph=\"{nameGraph}\" RETURN id(e1) as source, id(e2) as target',
                     {{graph:'cypher', write: true}})
                     YIELD nodeId, centrality
                     RETURN algo.getNodeById(nodeId).name as Entidad, centrality
@@ -44,14 +44,14 @@ class CypherQueries:
                     LIMIT 20;'''
         results, header = db.cypher_query(query)
         meta = f"Betweenness idGraph {nameGraph} relation {relation}"
-        print(meta, results)
+        #print(meta, results)
         logging.info(meta)
         self.exportCsv("_".join(meta.split(" ")), header, results)
         return results, header
 
     def pageRank_algo(self, nameGraph, relation, iterations, dampingFactor):
-        query = f'''CALL algo.pageRank.stream('MATCH (e:Entidad) WHERE e.idGraph="{nameGraph}" RETURN id(e) as id',
-                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph="{nameGraph}" AND e2.idGraph="{nameGraph}" RETURN id(e1) as source, id(e2) as target',
+        query = f'''CALL algo.pageRank.stream('MATCH (e:Entidad) WHERE e.idGraph=\"{nameGraph}\" RETURN id(e) as id',
+                    'MATCH (e1:Entidad)-[:{relation}]->(e2:Entidad) WHERE e1.idGraph=\"{nameGraph}\" AND e2.idGraph=\"{nameGraph}\" RETURN id(e1) as source, id(e2) as target',
                     {{graph:'cypher', write: true, iterations:{iterations}, dampingFactor:{dampingFactor}}})
                     YIELD nodeId, score
                     RETURN algo.getNodeById(nodeId).name as Entidad, score
@@ -59,7 +59,7 @@ class CypherQueries:
                     LIMIT 20;'''
         results, header = db.cypher_query(query)
         meta = f"PageRank idGraph {nameGraph} relation {relation} iterations {iterations} dampingFactor {dampingFactor}"
-        print(meta, results)
+        #print(meta, results)
         logging.info(meta)
         self.exportCsv("_".join(meta.split(" ")), header, results)
         return results, header
